25b3b066dbc9ac07e3f2db15878d93e6fb470d60
Trav <2472981+waltont8@users.noreply.github.com>
2022-12-04 06:20:39 +0000

    Create Day04.hs

---
+++ Day04.hs
@@ -0,0 +1,28 @@
+module Lib
+    ( advent
+    ) where
+
+import Control.Arrow
+import Data.List.Split
+
+advent :: IO ()
+advent = do
+        input <- fmap (lines >>> map parse >>> ((map score1 >>> sum) &&& (map score2 >>> sum))) $ readFile "input.txt"
+        print input
+
+parse :: String -> ((Int,Int),(Int,Int))
+parse s = ((read a,read b), (read c,read d))
+    where
+        [[a,b],[c,d]] = map (splitOn "-") (splitOn "," s)
+
+score1 ((a,b), (c,d))
+    | (a>=c) && (b<=d) = 1
+    | (c>=a) && (d<=b) = 1
+    | otherwise = 0
+
+score2 ((a,b), (c,d))
+    | (a>=c) && (a<=d) = 1
+    | (b>=c) && (b<=d) = 1
+    | (c>=a) && (c<=b) = 1
+    | (d>=a) && (d<=b) = 1
+    | otherwise = 0
